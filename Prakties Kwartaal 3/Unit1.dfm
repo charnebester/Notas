f6324d59b03517b1ee57d4e340895ed719441562
Charne Bester <charne.bester2009@gmail.com>
2025-09-09 05:34:49 +0200

    Klaar

--- Prakties Kwartaal 3/Unit1.dfm
+++ Prakties Kwartaal 3/Unit1.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 441
-  ClientWidth = 763
+  ClientHeight = 642
+  ClientWidth = 1057
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -31,6 +31,7 @@ object Form1: TForm1
     Height = 25
     Caption = 'VertoonOef1'
     TabOrder = 1
+    OnClick = btnVertoonOef1Click
   end
   object btnClearRED: TButton
     Left = 648
@@ -40,5 +41,23 @@ object Form1: TForm1
     Caption = 'Clear Red'
     TabOrder = 2
     OnClick = btnClearREDClick
+  end
+  object Button1: TButton
+    Left = 712
+    Top = 240
+    Width = 75
+    Height = 25
+    Caption = 'VertoonOef2'
+    TabOrder = 3
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 656
+    Top = 304
+    Width = 75
+    Height = 25
+    Caption = 'VertoonOef3'
+    TabOrder = 4
+    OnClick = Button2Click
   end
 end
--- Prakties Kwartaal 3/Unit1.pas
+++ Prakties Kwartaal 3/Unit1.pas
@@ -11,7 +11,12 @@ type
     redAfvoer: TRichEdit;
     btnVertoonOef1: TButton;
     btnClearRED: TButton;
+    Button1: TButton;
+    Button2: TButton;
     procedure btnClearREDClick(Sender: TObject);
+    procedure btnVertoonOef1Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -31,5 +36,148 @@ procedure TForm1.btnClearREDClick(Sender: TObject);
 begin
 redAfvoer.Lines.Clear;
 end;
+
+procedure TForm1.btnVertoonOef1Click(Sender: TObject);
+var
+tf : textfile;
+sEen : string;
+ iPos : integer;
+sID, sNaam : string;
+
+begin
+ try
+   AssignFile(tf , 'DataEen.txt') ;
+   Reset(tf);
+
+   while not Eof(tf) do
+   begin
+     Readln(tf , sEen);
+    // redAfvoer.Lines.Add(sEen);
+     iPos := Pos('|', sEen);
+     sID := Copy(sEen,1,iPos-1);
+     Delete(sEen,1,iPos);
+
+     iPos := Pos('~', sEen);
+     sNaam := copy(sEen,1,iPos-1);
+     Delete(sEen,1,iPos) ;
+
+     redAfvoer.Lines.Add(sNaam +#9 + sID);
+
+
+
+   end;
+
+ finally
+  CloseFile(tf);
+ end;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+tf : TextFile;
+sEen : string;
+iPos, I : integer;
+sID : string;
+ sNaam, sVan, sGetal, sX : string;
+
+
+begin
+ try
+ AssignFile(tf , 'DataTwee.txt')  ;
+ Reset(tf);
+ sGetal := '';
+ while not Eof(tf) do
+ begin
+ sGetal := ''; //elke keer moet die sterre of X'e vir elke persoon nuut beoooooogoionooo
+
+   Readln(tf , sEen);
+  //redAfvoer.lines.Add(sEen);
+   // more oef 3 add weer n btnVertoonOef3 is dit reg? oky maar ek dink ek moet iets met somme oef soos gem. Kyk na oef 3, gem is algewmene wisk jy kan dit self uitfigure maar oef 3 het so n bietjie wisk nag lekker slaap trots op jou ! dankie jy ook
+  iPos := Pos('|', sEen);
+  sID := copy(sEen,1,iPos-1)  ;
+  Delete(sEen,1,iPos);
+
+  iPos := Pos('~',sEen)     ;
+  sNaam := Copy(sEen,1,iPos-1);
+  delete(sEen,1,iPos);
+
+  iPos := pos('#', sEen);
+  sVan := copy(sEen,1,iPos-1);
+  Delete(sEen,1,iPos-1);
+
+  for I := 0 to Length(sVan) do
+  begin
+    sGetal := sGetal + 'X';
+
+  end;
+
+  redAfvoer.Lines.Add(UpperCase(sNaam[1]) +'.' + ' ' + sVan + ' ' + sGetal  ) ;
+
+  Delete(sGetal,1,Length(sVan)) ;
+
+
+
+ end;
+
+
+
+ finally
+  CloseFile(tf);
+ end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+tf : textfile;
+sEen, sID, sNaam, sVan, sLaaste, sEwe : string;
+iPos, I : integer;
+begin
+ try
+   AssignFile(tf , 'DataDrie.txt') ;
+   Reset(tf);
+
+   while not Eof(tf) do
+   begin
+     Readln(tf , sEen);
+     sLaaste := '';
+    //redAfvoer.Lines.Add(sEen);
+     iPos := Pos('|', sEen);
+     sID := copy(sEen,1,iPos-1);
+     Delete(sEen,1,iPos);
+
+      iPos := Pos('~',sEen)     ;
+  sNaam := Copy(sEen,1,iPos-1);
+  delete(sEen,1,iPos);
+
+  iPos := pos('#', sEen);
+  sVan := copy(sEen,1,iPos-1);
+  Delete(sEen,1,iPos-1);
+
+  for I := 1 to Length(sVan) do
+    begin
+     sLaaste := sVan[I] ;
+    end;
+
+    if StrToInt(sID)/2 = 0 then
+    begin
+    sEwe := 'Even ID';
+    end
+    else
+begin
+  sEwe := 'Odd ID';
+end;
+
+    redAfvoer.Lines.Add(sID[3] + sID[4] + ' ' + sLaaste + ' ' + sEwe )  ;
+
+
+   end;
+ finally
+   CloseFile(tf);
+
+
+   end;
+
+
+  end;
 
 end.
